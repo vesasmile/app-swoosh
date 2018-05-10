0d27ab63ae4d398e26f8af90cc6b87629bd072f1
Velibor Popovic <veliborpopovicsd@gmail.com>
2018-05-10 14:33:02 +0200

    some other changes

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSecondView(unwindsegue: UIStoryboardSegue){
+    
+    
     }
 
 
